4158f150914d3a9d69c0fb2097ecf81f1cc93870
opeyemibioku <opeyemibioku@gmail.com>
2023-10-31 12:32:46 +0100

    updated the cart functionalities

--- src/features/productsAPI.js
+++ src/features/productsAPI.js
@@ -3,7 +3,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const productsApi = createApi({
   reducerPath: "productsApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:3001",
+    baseUrl: "https://tech-marketplace-9c7e.onrender.com",
+    // baseUrl: "http://localhost:3001",
   }),
   endpoints: (builder) => ({
     getAllProducts: builder.query({
